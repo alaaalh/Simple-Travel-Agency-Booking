ea26ed924897a289c04904bb5464115306e5a506
alaaalh <alaaalh437@gmail.com>
2024-02-24 11:37:04 -0800

    setup ar and en languages

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,49 @@
-
 <template>
-    <div class="card">
-        <Menubar :model="items" />
-    </div>
+  <div class="card">
+    <Menubar :model="items" />
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
 
+const { setLocale, t, locale } = useI18n();
 const router = useRouter();
 
 const items = ref([
-    {
-        label: 'Home',
-        icon: 'pi pi-home',
-        command: () => router.push('/')
-    },
-    {
-        label: 'Language',
-        icon: "pi pi-flag-us",
-        items: [
-            {
-                label: 'English',
-                icon: 'pi pi-bolt'
-            },
-            {
-                label: 'Arabic',
-                icon: 'pi pi-server'
-            },
-        ]
-    },
-
+  {
+    label: t("home_title"),
+    icon: "pi pi-home",
+    command: () => router.push("/"),
+  },
+  {
+    label: t("language"),
+    icon: "pi pi-flag-us",
+    items: [
+      {
+        label: t("english"),
+        icon: "pi pi-flag-en",
+        command: () => {
+          setLocale("en");
+        },
+      },
+      {
+        label: t("arabic"),
+        icon: "pi pi-flag-ar",
+        command: () => {
+          setLocale("ar");
+        },
+      },
+    ],
+  },
 ]);
+
+watch(locale, () => {
+  items.value[1].items[0].label = t("english");
+  items.value[1].items[1].label = t("arabic");
+  items.value[0].label = t("home_title");
+  items.value[1].label = t("language");
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  title: "travel-agency-booking",
 
-  title: 'travel-agency-booking',
-  
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { hid: 'description', name: 'description', content: '' },
-    { name: 'format-detection', content: 'telephone=no' }
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { hid: "description", name: "description", content: "" },
+    { name: "format-detection", content: "telephone=no" },
   ],
 
   htmlAttrs: {
-    lang: 'en'
+    lang: "ar",
   },
- 
 
   devtools: { enabled: true },
-  modules: ["nuxt-primevue"],
+  modules: ["nuxt-primevue", "@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    langDir: "locales",
+    strategy: "prefix_except_default",
+    locales: [
+      {
+        code: "ar",
+        name: "Arabic",
+        file: "ar.json",
+      },
+      {
+        code: "en",
+        name: "English",
+        file: "en.json",
+        flag: '/flags/flag-en.png'
+      }
+      
+    ],
+    defaultLocal: "ar",
+    i18n: {
+      locales: ["ar", "en"],
+      defaultLocale: "ar",
+    },
+  },
 
   primevue: {
     cssLayerOrder: "reset,primevue",
   },
-  css: ["primevue/resources/themes/aura-light-green/theme.css", 'primeicons/primeicons.css'],
+  css: [
+    "primevue/resources/themes/aura-light-green/theme.css",
+    "primeicons/primeicons.css",
+  ],
 });
