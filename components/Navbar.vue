aa9aaafc4b5a51dca0a6e49721b38bb8c387905d
alaaalh <alaaalh437@gmail.com>
2024-02-24 09:09:38 -0800

    add default layout

---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div class="card">
+        <Menubar :model="items" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home',
+        command: () => router.push('/')
+    },
+    {
+        label: 'Language',
+        icon: "pi pi-flag-us",
+        items: [
+            {
+                label: 'English',
+                icon: 'pi pi-bolt'
+            },
+            {
+                label: 'Arabic',
+                icon: 'pi pi-server'
+            },
+        ]
+    },
+
+]);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+
+  title: 'travel-agency-booking',
+  
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: '' },
+    { name: 'format-detection', content: 'telephone=no' }
+  ],
+
+  htmlAttrs: {
+    lang: 'en'
+  },
+ 
+
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue"],
+
+  primevue: {
+    cssLayerOrder: "reset,primevue",
+  },
+  css: ["primevue/resources/themes/aura-light-green/theme.css", 'primeicons/primeicons.css'],
+});
