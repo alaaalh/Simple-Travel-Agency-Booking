44699c2192fed475964057fc065a8f4e9e0afa65
alaaalh <alaaalh437@gmail.com>
2024-02-24 15:54:55 -0800

    add css to form

--- components/RequestForm.vue
+++ components/RequestForm.vue
@@ -1,49 +1,101 @@
 <template>
-  <form class="flex justify-content-center">
-    <div class="p-col-12 p-md-6">
-      <Calendar v-model="data.dateFrom" placeholder="from"/>
-    </div>
-    <div class="p-col-12 p-md-6">
-    <Calendar v-model="data.dateTo" placeholder="to" />
-    </div>
-    <div>
-      <Dropdown
-        v-model="data.ticketClass"
-        :options="ticketClassOptions"
-        placeholder="Ticket class"
-      />
+  <form class="container">
+    <div class="form__field">
+      <div>
+        <Calendar v-model="data.dateFrom" placeholder="from" />
+        <Calendar v-model="data.dateTo" placeholder="to" />
+      </div>
 
-      <Dropdown
-        v-model="data.typeType"
-        :options="tripTypesOptions"
-        placeholder="Trip type"
-      />
+      <div class="form__field-dropdown">
+        <Dropdown
+          v-model="data.ticketClass"
+          :options="ticketClassOptions"
+          placeholder="Ticket class"
+        />
+
+        <Dropdown
+          v-model="data.typeType"
+          :options="tripTypesOptions"
+          placeholder="Trip type"
+        />
+      </div>
+
+      <div class="form__field-number">
+        <InputText
+          type="number"
+          v-model="passengers"
+          placeholder="Passengers"
+        />
+      </div>
+
+      <div class="submit-button">
+        <Button label="Submit"/>
     </div>
-
-    
-
-    <div class="card flex justify-content-center">
-        <InputText type="number" v-model="passengers" placeholder="Passengers"/>
     </div>
-
-        
   </form>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const ticketClassOptions = ref(['Economy', 'Business', 'First']);
-const tripTypesOptions = ref(['One-way', 'Round-trip'])
+const ticketClassOptions = ref(["Economy", "Business", "First"]);
+const tripTypesOptions = ref(["One-way", "Round-trip"]);
 
 const data = ref({
-    dateFrom: null,
-    dateTo: null,
-    ticketClass: null,
-    typeType: null,
-    passengers: null,
-})
-
+  dateFrom: null,
+  dateTo: null,
+  ticketClass: null,
+  typeType: null,
+  passengers: null,
+});
 </script>
 
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
 
+.container > div {
+  border: 1px solid gray;
+  width: 50%;
+  border-radius: 4px;
+  padding: 20px;
+}
+
+@media (max-width: 768px) {
+  .container > div {
+    width: 100%;
+  }
+}
+
+.container .p-calendar {
+  width: 50%;
+  padding: 0 5px;
+}
+
+.form__field-dropdown {
+  padding: 20px 5px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.container .p-dropdown {
+  width: 49.5%;
+}
+
+.form__field .p-inputtext {
+  width: 100%;
+}
+
+.form__field-number {
+  padding: 5px;
+}
+
+.submit-button{
+    display: flex;
+    justify-content: center;
+    margin-top: 25px;
+}
+</style>
