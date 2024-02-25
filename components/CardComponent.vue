e3959609755c5d268f0bcd3220fdcebf0f908ae8
alaaalh <alaaalh437@gmail.com>
2024-02-25 15:43:31 -0800

    add cards with responsive

---
+++ components/CardComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-container">
+    <Card style="width: 25rem; overflow: hidden; margin-top: 20px" v-for="item in items">
+      <template #header>
+        <img
+          alt="user header"
+          src="https://primefaces.org/cdn/primevue/images/usercard.png"
+        />
+      </template>
+      <template #title>{{ item.title }}</template>
+      <template #subtitle>{{ item.subTitle }}</template>
+      <template #content>
+        <p class="m-0">
+          {{ item.discription }}
+        </p>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup>
+    const props = defineProps(['items'])
+</script>
+
+<style scoped>
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 10px;
+  margin-top: 40px;
+}
+
+@media (max-width: 768px) {
+  .card-container {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 40px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,27 @@
 <template>
-    <RequestForm/>
+  <RequestForm />
+  <CardComponent :items="data"/>
 </template>
+
+<script setup>
+const data = ref([
+  {
+    title: "Card one",
+    subTitle: "first",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!",
+  },
+  {
+    title: "Card two",
+    subTitle: "second",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!",
+  },
+  {
+    title: "Card three",
+    subTitle: "third",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!",
+  },
+]);
+</script>
